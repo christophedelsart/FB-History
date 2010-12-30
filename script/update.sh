f602fa5760e88f01401f1a9d7484939da04b2bb2
Christophe Delsart <christophe.delsart@mimesis-republic.com>
2010-12-30 13:34:55 +0100

    Add Diff viewing & reverting if no need to commit

--- script/update.sh
+++ script/update.sh
@@ -8,6 +8,8 @@ IFS=,
 var1="`head -1 ../connect/all.js`"
 var2="`echo $var1 | awk -F" " '{print $5}'`"
 unset IFS
+echo "Viewing diff ..."
+git difftool -y -t opendiff ../connect/all.js
 echo "Ready to commit ..."
 read -p "Do you want to commit Yes / No ?" ans
 if [ "$ans" == "y" ] || [ "$ans" == "Y" ] || [ "$ans" == "Yes" ]
@@ -23,5 +25,12 @@ then
 		fi
 		
 else
-		echo "Local version updated ..."
+		read -p "Do you want to revert Yes / No ?" ans
+		if [ "$ans" == "y" ] || [ "$ans" == "Y" ] || [ "$ans" == "Yes" ]
+		then
+			echo "Reverting local version ..."
+			git checkout ../connect/all.js
+		else
+			echo "Local version updated & commited ..."
+		fi
 fi
