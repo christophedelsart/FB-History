3633087e9323e91949507e81f6ac4e028d222409
Christophe Delsart <christophe.delsart@mimesis-republic.com>
2011-08-17 10:31:59 +0200

    [Script Update] Add git log before commit & syntax fix when not reverting

--- script/update.sh
+++ script/update.sh
@@ -10,6 +10,8 @@ var2="`echo $var1 | awk -F" " '{print $5}'`"
 unset IFS
 echo "Viewing diff ..."
 git difftool -y -t opendiff ../connect/all.js
+echo "Git Log :"
+git log --oneline -n 5
 echo "Ready to commit ..."
 read -p "Do you want to commit Yes / No ?" ans
 if [ "$ans" == "y" ] || [ "$ans" == "Y" ] || [ "$ans" == "Yes" ]
@@ -31,6 +33,6 @@ else
 			echo "Reverting local version ..."
 			git checkout ../connect/all.js
 		else
-			echo "Local version updated & commited ..."
+			echo "Local version updated ..."
 		fi
 fi
